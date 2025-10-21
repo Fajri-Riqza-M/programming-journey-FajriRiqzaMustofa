f8530686dabfcea665ef8f263b0cbe7c051a9f73
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 07:47:30 +0700

    Add files via upload

---
+++ 4.2_fajririqza m_xpplg1/4.2_fajririqza m_xpplg1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2_fajririqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan umurmu: ");
+            int umur = int.Parse(Console.ReadLine());
+
+            if (umur >= 18)
+            {
+                Console.WriteLine("Anda sudah cukup umur untuk membuat KTP. ");
+            }
+        }
+    }
+}
