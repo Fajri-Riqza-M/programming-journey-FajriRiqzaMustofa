0a35763df0357dc20db9925b22f92f5969a8abcd
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 07:48:31 +0700

    Add files via upload

---
+++ 4.4_fajri riqza m_xpplg1/4.4_fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._4_fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan angkam: ");
+            int angka = int.Parse(Console.ReadLine());
+
+            if (angka >= 0)
+            {
+                Console.WriteLine("Angkamu positif ");
+            }
+            else
+            {
+                Console.WriteLine("Angkamu negatif ");
+            }
+        }
+    }
+}
