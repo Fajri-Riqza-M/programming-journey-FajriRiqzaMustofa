91637fa156291098e6c996ad785a0ebe9ccd5e37
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 08:45:55 +0700

    Add files via upload

---
+++ 7_tugas 2_5._fajri riqza m_xpplg1/7_tugas 2_5._fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_tugas_2_5._fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+            {
+                total += i;
+                i++;
+            }
+
+            Console.WriteLine("Total = " + total);
+        }
+    }
+}
