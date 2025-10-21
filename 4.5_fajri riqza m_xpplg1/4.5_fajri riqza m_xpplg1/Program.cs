6252c207ca1b4b6d59a8627e60d5a4c046c3bfea
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 07:48:50 +0700

    Add files via upload

---
+++ 4.5_fajri riqza m_xpplg1/4.5_fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._5_fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan umur anda: ");
+            int umur = int.Parse(Console.ReadLine());
+
+            if (umur >= 17)
+            {
+                Console.WriteLine("Anda sudah dewasa");
+            }
+            else
+            {
+                Console.WriteLine("Anda masih anak-anak");
+            }
+        }
+    }
+}
