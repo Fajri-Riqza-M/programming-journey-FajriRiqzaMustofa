db4bc60e7fa7af5ec2b2c7dad35ff06c72d53dd1
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 07:49:24 +0700

    Add files via upload

---
+++ 4.7_fajri riqza m_xpplg1/4.7_fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._7_fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai ujian: ");
+            int nilai = int.Parse(Console.ReadLine());
+
+            if (nilai >= 90)
+            {
+                Console.WriteLine("Grade A ");
+            }
+            else if (nilai >= 75)
+            {
+                Console.WriteLine("Grade B ");
+            }
+            else if (nilai >= 60)
+            {
+                Console.WriteLine("Grade C ");
+            }
+            else
+            {
+                Console.WriteLine("Grade D");
+            }
+        }
+    }
+}
