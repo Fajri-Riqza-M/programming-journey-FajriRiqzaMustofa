99314db235914577fc671d40c90205702609bf21
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 08:45:32 +0700

    Add files via upload

---
+++ 7_tugas 1_ 5._fajri riqza m_xpplg1/7_tugas 1_ 5._fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_tugas_1__5._fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program menampilkan perkalian 5 (tabel 5)
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("5 x " + i + " = " + (5 * i));
+            }
+        }
+    }
+}
