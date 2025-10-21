6fdb501c57cfdb6554bf6b35b14e042ab34f68ba
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 08:03:38 +0700

    Add files via upload

---
+++ Praktik7.2_FajriRiqzaMustofa_xpplg1/Praktik7.2_FajriRiqzaMustofa_xpplg1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._2_FajriRiqzaMustofa_xpplg1
+{
+    internal class Program
+    {
+        static void SapaNama(string nama)
+        {
+            Console.WriteLine("Hallo, " + nama + "!");
+            Console.WriteLine("Semangat beljar C# hari ini!");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nama anda: ");
+            string namasiswa = Console.ReadLine();
+            SapaNama(namasiswa); // Memanggil fungsi dengan argumen
+        }
+    }
+}
