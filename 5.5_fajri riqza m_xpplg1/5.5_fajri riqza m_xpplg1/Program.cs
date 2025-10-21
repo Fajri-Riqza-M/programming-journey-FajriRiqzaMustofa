938f822b47b2798106e4a21236eab75d85c1a242
Fajri-Riqza-M <fajri258713@student.smkn1kandeman.sch.id>
2025-10-21 08:00:08 +0700

    Add files via upload

---
+++ 5.5_fajri riqza m_xpplg1/5.5_fajri riqza m_xpplg1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5_fajri_riqza_m_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. instalasi variabel  perhitungan (counter)
+            int angka = 1;
+
+            Console.WriteLine("menampilkan angka 1 sampai 20:");
+
+            // 2. blok 'do-while'
+            do
+            {
+                // tampilkan nilai variabl 'angka' saat ini
+                Console.WriteLine(angka);
+
+                // tampilkan nilai 'angka' sebanyak 1(increment)
+                angka++;
+
+                // uji kondisi: loop berlanjut selama 'angka' kurang dari atau sama dngan 
+            } while (angka <= 20);
+
+            Console.WriteLine("selesai.");
+        }
+    }
+}
